717780586857ebfbe721b9226102770e8d52cba2
pdiakumis <pdiakumis@unimelb.edu.au>
2020-08-01 21:32:59 +1000

    setup caporal cli

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,35 @@ const fs = require("fs");
 const path = require("path");
 const shell = require("shelljs");
 const chalk = require("chalk");
-const cli = require("../src/cli").argv;
-const files = require("../src/files");
+const cwl = require("../src/cwl");
+const { description, version } = require("../package.json");
+const { program } = require("@caporal/core");
 
-files.write_workflow_json(cli.cwl, cli.versionName, cli.launchName);
+program
+  .description(description)
+  .version(version)
+  .command("cwl", "CWL-related functionality")
+  .argument("<cwl>", "Path to CWL file")
+  .option("-v, --versionName <string>", "Name of workflow version", {
+    default: "version1",
+  })
+  .option("-l, --launchName <string>", "Name of workflow launch", {
+    default: "launch1",
+  })
+  .action(({ logger, args, options }) => {
+    logger.info(
+      `CWL file: ${args.cwl}\nLaunchName: ${options.launchName}\nVersionName: ${options.versionName}`
+    );
+    // cwl.write_workflow_json(cli.cwl, cli.versionName, cli.launchName);
+  })
+
+  .command("run", "Run workflows")
+  .argument("<workflowName>", "Name of workflow version to run.")
+  .action(({ logger, args, options }) => {
+    logger.info(`Workflow name: ${args.workflowName}`);
+  });
+program.run();
+
+module.exports = {
+  argv: program,
+};
